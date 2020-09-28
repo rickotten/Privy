31ef152e581dfefd9be8f3c4c594673e84d8fcff
rickotten <ottenrichie@gmail.com>
2020-09-27 23:15:33 -0400

    Added basic configuration for heroku deployments

--- Privy/settings.py
+++ Privy/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frontend'
 ]
 
 MIDDLEWARE = [
@@ -117,4 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
--- Privy/urls.py
+++ Privy/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+import frontend.views
 
 urlpatterns = [
+    path('', frontend.views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
