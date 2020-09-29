a49c9eedced163e7600bd1f82ce3ba7e6a203eb9
rickotten <ottenrichie@gmail.com>
2020-09-28 22:50:21 -0400

    added accounts app for registration

--- Privy/settings.py
+++ Privy/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'frontend'
+    'frontend',
+    'accounts'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
