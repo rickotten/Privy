c1a24ad9fbcd3acc3cdf7fa516478fcedeec178a
rickotten <ottenrichie@gmail.com>
2020-09-28 23:56:49 -0400

    Transitioned from default User to custom User Model

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from .models import User
 from django.contrib.auth import authenticate
 
 # User Serializer
