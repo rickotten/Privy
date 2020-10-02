cec7e43f8f108720f4bef30fcf8b46540969b1df
rickotten <ottenrichie@gmail.com>
2020-10-02 16:34:35 -0400

    front-end authentication complete

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -7,11 +7,15 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    LOGOUT_SUCCESS
+    LOGOUT_SUCCESS,
+    REGISTER_FAIL,
+    REGISTER_SUCCESS
 } from './types';
 
 // CHECK TOKEN & LOAD USER
 export const loadUser = () => (dispatch, getState) => {
+    dispatch({ type: USER_LOADING });
+
     const config = tokenConfig(getState);
     axios.get('/api/auth/user', config)
         .then(res => {
@@ -49,6 +53,32 @@ export const login = (username, password) => (dispatch) => {
             dispatch(returnErrors(err.response.data, err.response.status));
             dispatch({
                 type: LOGIN_FAIL,
+            })
+        });
+}
+
+// REGISTER USER
+export const register = ({ username, password, email }) => (dispatch) => {
+    // Headers 
+    const config = {
+        headers: {
+            'Content-type': 'application/json'
+        }
+    };
+
+    // Request Body
+    const body = JSON.stringify({ username, email, password });
+
+    axios.post('/api/auth/register', body, config)
+        .then(res => {
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            });
+        }).catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({
+                type: REGISTER_FAIL,
             })
         });
 }
--- frontend/src/components/registration/RegistrationForm.js
+++ frontend/src/components/registration/RegistrationForm.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { register } from '../../actions/auth';
+import { createMessage } from '../../actions/errors';
 
 export class RegistrationForm extends Component {
     state = {
@@ -9,14 +13,33 @@ export class RegistrationForm extends Component {
         password2: ''
     }
 
+    static propTypes = {
+        register: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool
+    }
+
     onSubmit = e => {
         e.preventDefault();
-        console.log('submit')
+        const { username, email, password, password2 } = this.state;
+        if (password !== password2) {
+            this.props.createMessage({ passwordNotMatch: 'Passwords do not match' });
+        } else {
+            console.log('hello');
+            const newUser = {
+                username,
+                email,
+                password
+            }
+            this.props.register(newUser);
+        }
     }
 
     onChange = e => this.setState({ [e.target.name]: e.target.value });
 
     render() {
+        if (this.props.isAuthenticated) {
+            return <Redirect to="/" />
+        }
         const { username, email, password, password2 } = this.state;
         return (
             <div className="col-md-6 m-auto">
@@ -126,4 +149,8 @@ export class RegistrationForm extends Component {
     }
 }
 
-export default RegistrationForm
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { register, createMessage })(RegistrationForm)
