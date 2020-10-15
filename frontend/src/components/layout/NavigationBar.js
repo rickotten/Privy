9fb1cfc962dc4d7d3f65a10d76e770a4f9060412
Dylan Hamm <dylhamm7@gmail.com>
2020-10-15 18:57:38 -0400

    password now actually resets and navbar added once user is logged in

---
+++ frontend/src/components/layout/NavigationBar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+export class NavigationBar extends Component {
+
+    render() {
+        return (
+            <div>
+                <Navbar bg="primary" variant="dark">
+                    <Navbar.Brand>P R I V Y</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link>Features</Nav.Link>
+                        <Nav.Link>Pricing</Nav.Link>
+                        <Nav.Link>Logout</Nav.Link>
+                    </Nav>
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+export default NavigationBar;
