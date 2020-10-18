ecfc278a68fa741d3042e2d41114b9806f30808a
rickotten <ottenrichie@gmail.com>
2020-10-18 14:35:50 -0400

    Basic homepage template

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -8,11 +8,10 @@ import AlertTemplate from "react-alert-template-basic";
 import Alerts from './layout/Alerts';
 import RegistrationForm from "./registration/RegistrationForm";
 import LoginForm from "./login/LoginForm";
-import User from "./user/User";
+// import User from "./user/User";
 import PrivateRoute from "./common/PrivateRoute";
 import ForgotCredentialsForm from "./login/ForgotCredentialsForm";
-
-import GoogleOAuth from './oauth/GoogleOAuth';
+import HomePage from "./layout/HomePage";
 
 import { Provider } from 'react-redux';
 import store from '../store';
@@ -38,7 +37,7 @@ export class App extends Component {
               <Alerts />
               <div className="container">
                 <Switch>
-                  <PrivateRoute exact path="/" component={User} />
+                  <PrivateRoute exact path="/" component={HomePage} />
                   <Route exact path="/register" component={RegistrationForm} />
                   <Route exact path="/login" component={LoginForm} />
                   <Route exact path="/forgot" component={ForgotCredentialsForm} />
