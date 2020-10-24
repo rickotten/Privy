43fbaad5e95a8a6d4db5c4d76636cc0d6cf7ab70
Dusteen99 <truesk8er01@hotmail.com>
2020-10-24 13:14:05 -0400

    Testing some changes

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -3,12 +3,35 @@ import { returnErrors } from './errors';
 import { tokenConfig } from './auth';
 
 import { 
+    USER_POSTS_CREATE_SUCESS,
+    USER_POSTS_CREATE_FAILURE,
     USER_POSTS_GET_SUCESS,
     USER_POSTS_GET_FAILURE,
     USER_POSTS_GET_LOADING,
     UPDATE_USER_POST_FAILURE,
     UPDATE_USER_POST_SUCCESS
  } from "./types";
+
+//CREATE A USER POST maybe done?
+export const create_user_post = (text_post) => (dispatch, getState)  => {
+    const config = tokenConfig(getState);
+    const body = JSON.stringify(text_post);
+
+    //maybe change axios states?
+    //axios.post(`/api/auth/userposts/${post.id}`, body, config)
+    axios.post(`/api/auth/userposts`, body, config)
+        .then(res => {
+            dispatch({
+                type: USER_POSTS_CREATE_SUCESS,
+                payload: res.data
+            })
+        }).catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({
+                type: USER_POSTS_CREATE_FAILURE,
+            })
+        });
+}
 
 // GET POSTS OF AUTHENTICATED USER
 export const get_user_posts = () => (dispatch, getState) => {
