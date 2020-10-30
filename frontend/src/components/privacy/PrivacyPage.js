37bda533b3a8083a75eedf5c6902288697dc4363
josh <joecody@iu.edu>
2020-10-30 16:05:21 -0400

    Moved privacy page to React Component

---
+++ frontend/src/components/privacy/PrivacyPage.js
@@ -0,0 +1,33 @@
+import react, {Component} from 'react';
+
+
+export class PrivacyPage extends Component {
+    
+
+    render(){
+        return(
+            <div className = "privPage">
+                <title>Privacy Page</title>
+                <h1>Here you can adjust your privacy settings</h1>
+                <h1>Privacy</h1>
+
+                <h2>Show email?</h2>
+                <sub>This option will show your email on your page</sub>
+                <label class="switch">
+                    <input type="checkbox"></input>
+                    <span class="slider round"></span>
+                </label>
+
+                <h3>Private page?</h3>
+                <sub>This option will hide your page from people who do not follow you</sub>
+                <label class="switch">
+                    <input type="checkbox"></input>
+                    <span class="slider round"></span>
+                </label>
+
+                <button type="button">Save</button>
+            </div>
+        );
+    }
+
+}
