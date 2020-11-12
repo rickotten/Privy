f40c5b7817dd4c0100f7cda42b5178915f1a560a
Dylan Hamm <dylhamm7@gmail.com>
2020-11-11 21:37:49 -0500

    cleaned up login page

--- frontend/src/components/login/LoginForm.js
+++ frontend/src/components/login/LoginForm.js
@@ -36,48 +36,58 @@ export class LoginForm extends Component {
         }
         const { username, password } = this.state;
         return (
-            <div className="col-md-6 m-auto" >
-                <div className="card card-body mt-5">
-                    <h2 className="text-center">Login</h2>
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <label>Username</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="username"
-                                onChange={this.onChange}
-                                value={username}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                name="password"
-                                onChange={this.onChange}
-                                value={password}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-primary">
-                                Login
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6" >
+                        <div className="card card-body mt-5">
+                            <h2 className="text-center">Login to Privy!</h2>
+                            <form onSubmit={this.onSubmit}>
+                                <div className="form-group">
+                                    <label>Username</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="username"
+                                        onChange={this.onChange}
+                                        value={username}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        name="password"
+                                        onChange={this.onChange}
+                                        value={password}
+                                    />
+                                </div>
+                                <div className="form-group text-center">
+                                    <button type="submit" className="btn btn-success btn-lg">
+                                        Login!
                             </button>
+                                </div>
+                                <p>
+                                    Don't have an account? <Link className="linkColor" to="/register">Register</Link>
+                                </p>
+                                <p>
+                                    Forgot your username or password? <Link className="linkColor" to="/forgot">Forgot</Link>
+                                </p>
+                                <p className="text-center">
+                                    <GoogleOAuth />
+                                </p>
+                                <p className="text-center">
+                                    <FacebookOAuth />
+                                </p>
+                            </form>
                         </div>
-                        <p>
-                            Don't have an account? <Link to="/register">Register</Link>
-                        </p>
-                        <p>
-                            <GoogleOAuth/>
-                        </p>
-                        <p>
-                            <FacebookOAuth/>
-                        </p>
-                        <p>
-                            Forgot your username or password? <Link to="/forgot">Forgot</Link>
-                        </p>
-                    </form>
+                    </div>
+                    <div className="col-md-6">
+                        <br/>
+                        <br/>
+                        <br/>
+                        <img src="..\static\images\loginOuthouse.png" alt="login outhouse" width="80%" height="80%"></img>
+                    </div>
                 </div>
             </div>
         )
@@ -88,4 +98,4 @@ const mapStateToProps = state => ({
     isAuthenticated: state.auth.isAuthenticated
 })
 
-export default connect(mapStateToProps, { login})(LoginForm)
+export default connect(mapStateToProps, { login })(LoginForm)
