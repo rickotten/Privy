3185e65b292c4e3d825c197c6baa8d2226dec4bc
rickotten <32284386+rickotten@users.noreply.github.com>
2020-11-29 12:10:43 -0500

    Prevent redirection after refresh (#34)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,41 @@
 // FOR REDUX
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initialState = {};
+// Prevent reset on refresh
+function saveToLocalStorage(state) {
+  console.log("Saving")
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (err) {
+    console.log(err)
+    return undefined;
+  }
+}
 
-const middleware = [thunk];
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return {};
+    return JSON.parse(serializedState)
+  } catch (err) {
+    console.log(err)
+    return undefined;
+  }
+}
 
+const persistedState = loadFromLocalStorage();
+
+const sagaMiddleWare = createSagaMiddleware();
 const store = createStore(
   rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+  persistedState,
+  composeWithDevTools(applyMiddleware(thunk, sagaMiddleWare)))
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export default store;
