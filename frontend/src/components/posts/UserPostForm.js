2729265d5472e13b5cc30b776ee4750b97a664d3
Dylan Hamm <dylhamm7@gmail.com>
2020-12-05 20:37:53 -0500

    button changes

--- frontend/src/components/posts/UserPostForm.js
+++ frontend/src/components/posts/UserPostForm.js
@@ -3,7 +3,7 @@ import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form'
-import { create_user_post} from '../../actions/posts';
+import { create_user_post } from '../../actions/posts';
 import NavigationBar from '../layout/NavigationBar';
 import { Grid } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
@@ -23,7 +23,7 @@ export class UserPostForm extends Component {
 
     onSubmit = e => {
         e.preventDefault();
-        this.props.create_user_post(this.state.text_post, this.state.media, this.props.page_id); 
+        this.props.create_user_post(this.state.text_post, this.state.media, this.props.page_id);
     }
 
     onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -34,37 +34,46 @@ export class UserPostForm extends Component {
     render() {
         const { text_post, media } = this.state;
         return (
-            <Grid>
-                <Paper>
-                    <div className="form-group">
-                            <form onSubmit={this.onSubmit}>
-                                {/* The text for the user's post */}
+            <div className="container">
+            <div className="row">
+                <div className="col-6-md">
+                    <Grid>
+                        <Paper>
+                            <div className="form-group text-center">
+                                <form onSubmit={this.onSubmit}>
+                                    {/* The text for the user's post */}
 
-                                        <Form.Group controlId="exampleForm.ControlInput1">
-                                            
-                                            <Form.Control type="text" placeholder="What do you want to say?"
-                                                type="text"
-                                                name="text_post"
-                                                onChange={this.onChange}
-                                                value={text_post} 
-                                                style={{height: 40}}/>
+                                    <Form.Group controlId="exampleForm.ControlInput1">
 
-                                        </Form.Group>
+                                        <Form.Control type="text" placeholder="What do you want to say?"
+                                            type="text"
+                                            name="text_post"
+                                            onChange={this.onChange}
+                                            value={text_post}
+                                            style={{ height: 40 }} />
 
-                                        <Form.Group>
-                                            <Form.File label="Attach a File:" 
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.File label="Attach a File:"
                                             type="file"
                                             name="media"
                                             onChange={this.onChangeImage}
-                                            />                   
-                                        </Form.Group>
-                                <a href = "/">
-                                    <button style= {{fontSize:15, height:35, width:115}} type="submit" className="btn btn-primary">Post</button>
-                                </a>
-                            </form>
-                        </div>
+                                        />
+                                    </Form.Group>
+                                    <a href="/">
+                                        <button style={{ fontSize: 15, height: 35, width: 115 }} type="submit" className="btn btn-primary btn-success btn-lg">Post!</button>
+                                    </a>
+                                </form>
+                            </div>
                         </Paper>
-                </Grid>
+                    </Grid>
+                </div>
+                <div className="col-6-md">
+                    hello
+                </div>
+            </div>
+            </div>
         )
     }
 }
