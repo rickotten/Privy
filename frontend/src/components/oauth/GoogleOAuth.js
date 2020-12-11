bb8f07498b61ab2c7a7e4909502b3e96d62ca318
rickotten <ottenrichie@gmail.com>
2020-12-11 02:22:16 -0500

    frontend migration of keys to env

--- frontend/src/components/oauth/GoogleOAuth.js
+++ frontend/src/components/oauth/GoogleOAuth.js
@@ -18,7 +18,9 @@ export class GoogleOAuth extends Component {
         if (!this.props.isAuthenticated) {
             return (
                 <GoogleLogin
-                    clientId="449793272806-rmmuu0tqqflvroe6r09bbul8e6scndbq.apps.googleusercontent.com"
+                    // DEPCRECATED CLIENT_ID
+                    // clientId="449793272806-rmmuu0tqqflvroe6r09bbul8e6scndbq.apps.googleusercontent.com"
+                    clientId={process.env.GOOGLE_OAUTH_CLIENT_ID}
                     buttonText="Google"
                     onSuccess={this.responseGoogle}
                     onFailure={this.responseGoogle}
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,9 @@
+const path = require("path");
+const webpack = require('webpack');
+const dotenv = require('dotenv').config( {
+  path: path.join(__dirname, '.env')
+});
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      { test: /\.css$/, loader: "style-loader!css-loader" }
-    ],
-  },
-
   module: {
     rules: [{
       test: /.jsx?$/,
@@ -24,6 +16,12 @@ module.exports = {
       test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
       loader: 'url-loader?limit=100000' }]
   },
-
-
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": dotenv.parsed
+    }),
+  ],
+  node: {
+    fs: "empty"
+  }
 };
